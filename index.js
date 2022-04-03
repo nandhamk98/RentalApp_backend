d3bf797e4c8a163735e7ba986a516002a7878e56
Nandhakumar <nandhakumarmk98@gmail.com>
2022-04-03 16:03:41 +0530

    All changes are revereted

--- index.js
+++ index.js
@@ -36,85 +36,85 @@ const createConnection = async () => {
 
 export const client = await createConnection();
 
-// app.post("/razorpay", async (req, res) => {
-//   const payment_capture = 1;
-//   const { amount } = req.body;
-//   const currency = "INR";
+app.post("/razorpay", async (req, res) => {
+  const payment_capture = 1;
+  const { amount } = req.body;
+  const currency = "INR";
 
-//   const options = {
-//     amount: amount * 100,
-//     currency,
-//     receipt: "qwsaq1",
-//     payment_capture,
-//   };
+  const options = {
+    amount: amount * 100,
+    currency,
+    receipt: "qwsaq1",
+    payment_capture,
+  };
 
-//   try {
-//     const response = await razorpay.orders.create(options);
-//     console.log(response);
-//     res.json({
-//       id: response.id,
-//       currency: response.currency,
-//       amount: response.amount,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
+  try {
+    const response = await razorpay.orders.create(options);
+    console.log(response);
+    res.json({
+      id: response.id,
+      currency: response.currency,
+      amount: response.amount,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-// app.post("/signup", async (req, res) => {
-//   const { username, password } = req.body;
-//   const hashedPassword = await getHashedPassword(password);
+app.post("/signup", async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await getHashedPassword(password);
 
-//   const data = await client
-//     .db("rental_app")
-//     .collection("users")
-//     .findOne({ username: username });
+  const data = await client
+    .db("rental_app")
+    .collection("users")
+    .findOne({ username: username });
 
-//   if (!data) {
-//     const createUser = await client
-//       .db("rental_app")
-//       .collection("users")
-//       .insertOne({ username: username, password: hashedPassword });
+  if (!data) {
+    const createUser = await client
+      .db("rental_app")
+      .collection("users")
+      .insertOne({ username: username, password: hashedPassword });
 
-//     res.send({ message: "successfully created" });
-//   } else {
-//     res.send({ message: "username exists" });
-//   }
-// });
+    res.send({ message: "successfully created" });
+  } else {
+    res.send({ message: "username exists" });
+  }
+});
 
-// app.post("/login", async (req, res) => {
-//   const { username, password } = req.body;
-//   const hashedPassword = await getHashedPassword(password);
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await getHashedPassword(password);
 
-//   // console.log(hashedPassword);
+  // console.log(hashedPassword);
 
-//   const data = await client
-//     .db("rental_app")
-//     .collection("users")
-//     .findOne({ username: username });
+  const data = await client
+    .db("rental_app")
+    .collection("users")
+    .findOne({ username: username });
 
-//   // console.log(data);
+  // console.log(data);
 
-//   if (!data) {
-//     res.send({ message: "invalid Credentails" });
-//   } else {
-//     const isCorrect = await bcrypt.compare(password, hashedPassword);
-//     console.log(isCorrect);
-//     if (isCorrect) {
-//       res.send({ message: "logged in successful" });
-//     } else {
-//       res.send({ message: "incorrect password" });
-//     }
-//   }
-//   res.send({ message: "successfully created" });
-// });
+  if (!data) {
+    res.send({ message: "invalid Credentails" });
+  } else {
+    const isCorrect = await bcrypt.compare(password, hashedPassword);
+    console.log(isCorrect);
+    if (isCorrect) {
+      res.send({ message: "logged in successful" });
+    } else {
+      res.send({ message: "incorrect password" });
+    }
+  }
+  res.send({ message: "successfully created" });
+});
 
-// const getHashedPassword = async (password) => {
-//   const salt = await bcrypt.genSalt(10);
-//   const hashedPassword = await bcrypt.hash(password, salt);
-//   console.log("q2w2e ", hashedPassword);
-//   return hashedPassword;
-// };
+const getHashedPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  console.log("q2w2e ", hashedPassword);
+  return hashedPassword;
+};
 
 app.listen(PORT, () => {
   console.log(`The server started in ${PORT}`);
