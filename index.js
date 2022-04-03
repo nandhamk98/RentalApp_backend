b4fad02a044439faa75f7fac0dbd0bc06c192325
Nandhakumar <nandhakumarmk98@gmail.com>
2022-04-03 15:58:08 +0530

    reverted back

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Razorpay } from "Razorpay";
+import Razorpay from "razorpay";
 import { productsRouter } from "./Routes/productRouter.js";
 // import bcrypt from "bcrypt";
 
